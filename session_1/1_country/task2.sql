526db6fe1382f3d4eebcee362c93e7d9aaa54db6
Syeda <clwr0502@leeds.ac.uk>
2026-01-30 14:40:21 +0000

    Task 1 and 2 complete

--- session_1/1_country/task2.sql
+++ session_1/1_country/task2.sql
@@ -16,4 +16,8 @@
 --3. SELECT country FROM countries ORDER BY AreaSqMi DESC LIMIT 1;
 --4. SELECT country FROM countries WHERE Continent = 'Africa' ORDER BY AreaSqMi ASC LIMIT 3;
 --5. SELECT country FROM countries ORDER BY LiteracyPercent ASC LIMIT3;
-
+--6. SELECT country FROM countries WHERE country LIKE 'C%' ORDER BY GDPPerCapita DESC LIMIT 3;
+--7. SELECT country FROM countries WHERE InfantMortalityPer1000 > 50 ORDER BY LitereacyPercent DESC LIMIT 1;
+--8. SELECT country FROM countries WHERE GDPPerCapita > 10000 ORDER BY PhonesPer1000 ASC LIMIT 5;
+--9. SELECT country FROM countries WHERE Continent = 'South America' AND CoastlineRatio = 0 ORDER BY GDPPerCapita DESC LIMIT 1;
+--10. SELECT country FROM countries ORDER BY Country DESC LIMIT 10;
